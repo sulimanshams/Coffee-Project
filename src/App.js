a36c31bd81ec0387b51e499e47256d7b5273e1d1
sulimanshams <sulimanshamsn191@gmail.com>
2025-07-24 10:52:38 +0430

    Second commit of the Coffee  project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import myImage from "D:/Coffe-Project-Excersise/coffee-project/src/assets/bg-cafe-lg.jpg"; 
 
 function App() {
-  
+  return (
+    <div className="app">
+      <img src={myImage} alt="banner" className="top-banner" />
+      
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      </h1>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="bg-container">
+      <App />
+    </div>
   </React.StrictMode>
 );
 
