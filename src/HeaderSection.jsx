10681c57223483b0ec915382a46a098ca8d829bd
sulimanshams <sulimanshamsn191@gmail.com>
2025-07-27 13:36:05 +0430

    Text Position

--- src/HeaderSection.jsx
+++ src/HeaderSection.jsx
@@ -3,21 +3,23 @@ import {Box} from "@mui/material"
 const SectionHeader= () =>{
   return (
     <>
-<Box sx={{mt:-147 , ml:92}}>
+<Box sx={{mt:-147 , ml:100, position:"relative"}}>
     <img 
         src="/vector.svg"
         alt="Vector"
         />
         </Box>
-        <Box sx={{color:"#FEF7EE", fontSize:"32px" , fontFamily:"'DM Sans', sans-serif", marginTop:"-1010px"}}>
+        <Box sx={{display:"inline-block", whiteSpace:"nowrap" ,color:"#FEF7EE", fontSize:"32px" , fontFamily:"'DMSans_18pt-Medium'", marginTop:"-1150px" , marginLeft:"55px",
+          position:"absolute"
+        }}>
         <h2>Our Collection</h2>
          </Box>
-        <Box>
+        <Box sx={{color:"#6F757C" , mr:"200px" , width:"700px"}}>
         <p className="text-gray-400 max-w-xl mx-auto mt-3 relative z-10">
        Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
     </Box>
 </>
-)
+) 
 }
 
 
