e337ae9d736613af21f4c1da8b1db6851a131929
sulimanshams <sulimanshamsn191@gmail.com>
2025-07-27 10:16:34 +0430

    Font

--- src/HeaderSection.jsx
+++ src/HeaderSection.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-
+import {Box} from "@mui/material"
 const SectionHeader= () =>{
   return (
-    <div className="relative text-center mb-10 py-10">
-        <img
+    <>
+<Box sx={{mt:-147 , ml:92}}>
+    <img 
         src="/vector.svg"
         alt="Vector"
         />
-        <h2 className="text-3xl font-semibold text-white relative z-10">Our Collection</h2>
+        </Box>
+        <Box sx={{color:"#FEF7EE", fontSize:"32px" , fontFamily:"'DM Sans', sans-serif", marginTop:"-1010px"}}>
+        <h2>Our Collection</h2>
+         </Box>
+        <Box>
         <p className="text-gray-400 max-w-xl mx-auto mt-3 relative z-10">
        Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
-    </div>
-
+    </Box>
+</>
 )
 }
 
