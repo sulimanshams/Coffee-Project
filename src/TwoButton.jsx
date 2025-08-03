ce9ab4219c4c2afb6a9ad550f4dc08101a7391c6
sulimanshams <sulimanshamsn191@gmail.com>
2025-08-03 12:14:17 +0430

    Style

--- src/TwoButton.jsx
+++ src/TwoButton.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 const TwoButton =() => {
     const [selected , setSelected] = useState(null)
     return (
-        <Box display="flex" gap={2} position="absolute" marginTop="-820px">
+        <Box display="flex" gap={2} position="absolute" marginTop="-750px">
             <Button variant="contained"
             onClick={()=>setSelected('btn1')}
             sx={{
